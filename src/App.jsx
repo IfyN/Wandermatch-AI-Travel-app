e1297fc41b70bc19432f284deeb9dcde65bd29ca
Ifeoma N <31158530+IfyN@users.noreply.github.com>
2025-11-20 16:32:36 +0000

    feature: add trip type and duration dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 function App() {
-
   const [filters, setFilters] = useState({
-    budget: '',
-    type: '',
-    duration: '',
-    moods: []
+    budget: "",
+    type: "",
+    duration: "",
+    moods: [],
   });
 
-  console.log('Current filters:', filters); 
+  console.log("Current filters:", filters);
 
   return (
-  <div>
-    <h1>WanderMatch</h1>
-    <p>Find your perfect weekend escape</p>
-
     <div>
-      <label>Budget:</label>
-      <select 
-  value={filters.budget} 
-  onChange={(e)=> setFilters({ ...filters, budget: e.target.value})}>
-        <option value="">Any Budget</option>
-        <option value="low">Low (£50-150)</option>
-        <option value="medium">Medium (£150-350)</option>
-        <option value="high">High (£350+)</option>
-      </select>
+      <h1>WanderMatch</h1>
+      <p>Find your perfect weekend escape</p>
+
+      <div>
+        <label>Budget:</label>
+        <select
+          value={filters.budget}
+          onChange={(e) => setFilters({ ...filters, budget: e.target.value })}
+        >
+          <option value="">Any Budget</option>
+          <option value="low">Low (£50-150)</option>
+          <option value="medium">Medium (£150-350)</option>
+          <option value="high">High (£350+)</option>
+        </select>
+      </div>
+      <div>
+        <label>Trip type: </label>
+        <select
+          value={filters.type}
+          onChange={(e) => setFilters({ ...filters, type: e.target.value })}
+        >
+          <option value="">Any Type</option>
+          <option value="nature"> Nature</option>
+          <option value="city">City</option>
+          <option value="adventure">Adventure</option>
+          <option value="beach">Beach</option>
+          <option value="relaxation">Relaxation</option>
+        </select>
+      </div>
+       <div>
+        <label>Duration: </label>
+        <select
+          value={filters.duration}
+          onChange={(e) => setFilters({ ...filters, duration: e.target.value })}
+        >
+          <option value="">Any Type</option>
+          <option value="day"> Day Trip</option>
+          <option value="weekend">Weekend</option>
+          <option value="week">Week</option>
+        
+        </select>
+      </div>
     </div>
-  </div>
-)
+  );
 }
 
 export default App;
